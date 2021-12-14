5a531444ddef1fe42b9c1ad282dafac8cccf10b1
Szotkowski <83296342+Szotkowski@users.noreply.github.com>
2021-12-14 21:11:26 +0100

    Delete Kája directory

--- Kája/js.js
+++
@@ -1,9 +0,0 @@
-function Kosik() {
-    let Div = document.getElementById("pocet");
-    let cena = document.getElementById("cena");
-    alert(Div);
-    if (Div > 0) {
-        Div = abs(Div);
-        Div.innerHTML = Div;
-    }
-}
