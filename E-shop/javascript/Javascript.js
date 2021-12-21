82b15ac25a8a0f036a118c97d93242993dc3c446
Szotkowski <83296342+Szotkowski@users.noreply.github.com>
2021-12-21 20:12:56 +0100

    commit

--- E-shop/javascript/Javascript.js
+++ E-shop/javascript/Javascript.js
@@ -1,11 +1,9 @@
 function Kosik() {
     var count = document.getElementById("pocet").value;
     sessionStorage.setItem("count", count);
-    console.log(count);
 }
 
 function Kosik1() {
     var newcount = sessionStorage.getItem("count");
     document.getElementById("pocet").setAttribute("value", newcount);
-    console.log(newcount);
 }
