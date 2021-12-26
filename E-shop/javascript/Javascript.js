801852f8c4a0e670dd1dcd3967b959e022ceccd7
Szotkowski <83296342+Szotkowski@users.noreply.github.com>
2021-12-26 19:52:27 +0100

    commit

--- E-shop/javascript/Javascript.js
+++ E-shop/javascript/Javascript.js
@@ -16,42 +16,17 @@ function Cena() {
     document.getElementById("cena").innerText=price*oneprice;
 }
 
-function sendEmail() {
-    Email.send({
-      Host: "smtp.gmail.com",
-      Username: "mikiszotkowski@gmail.com",
-      Password: "ahoj@nemr5",
-      To: 'mikiszotkowski@gmail.com',
-      From: "mikiszotkowski@gmail.com",
-      Subject: "Smartcard",
-      Body: "Some text",
-    })
-      .then(function (message) {
-        alert("mail sent successfully")
-      });
-  }
-
-  var ConvertedFileStream;
-  var g_recipient;
-  var g_subject;
-  var g_body;
-  var g_attachmentname;
-  
-  
-  function SendMailItem(p_recipient, p_subject, p_body, p_file, p_attachmentname, progressSymbol) {
-  
-      // Email address of the recipient 
-      g_recipient = p_recipient;
-  
-     // Subject line of an email
-      g_subject = p_subject;
-  
-     // Body description of an email
-      g_body = p_body;
-  
-      // attachments of an email
-      g_attachmentname = p_attachmentname;
-  
-      SendC360Email(g_recipient, g_subject, g_body, g_attachmentname);
-  
-}
+function sendEmail() { 
+	Email.send({ 
+		Host: "smtp.seznam.cz",
+		Username: "smartcard@post.cz",
+		Password: "Smartcard123",
+		To: 'smartcard@post.cz',
+		From: "smartcard@post.cz",
+		Subject: "Smartcard",
+		Body: "Well that was easy!!"
+	}) 
+		.then(function (message) { 
+		alert("Mail has been sent successfully") 
+		}); 
+	}
