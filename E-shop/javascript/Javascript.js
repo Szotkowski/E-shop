632dd80c82ec5e165d3ab7228bcdc0808965aa16
Szotkowski <83296342+Szotkowski@users.noreply.github.com>
2022-01-10 23:04:34 +0100

    commit

--- E-shop/javascript/Javascript.js
+++ E-shop/javascript/Javascript.js
@@ -14,40 +14,44 @@ function Kosik1() {
 function Cena() {
     var price = sessionStorage.getItem("count1");
     document.getElementById("cena").innerText=price*oneprice+" Kč";
-	sessionStorage.setItem("price", price*oneprice);
+	sessionStorage.setItem("price", price);
+}
+
+function Cena1() {
+	if (sessionStorage.getItem("price") > 0) {
+		location.href='Formulář.html';
+	}
 }
 
 function sendEmail() {
-  var jmeno = document.getElementById("jmeno").value;
-  var prijmeni = document.getElementById("prijmeni").value;
-  var ulice = document.getElementById("ulice").value;
-  var PSC = document.getElementById("PSC").value;
-  var mesto = document.getElementById("mesto").value;
-  var email = document.getElementById("email").value;
-  var telefon = document.getElementById("telefon").value;
-    Email.send({ 
-    	Host: "smtp.seznam.cz",
-		Username: "smartcard@post.cz",
-		Password: "Smartcard123",
-		To: 'smartcard@post.cz',
-		From: "smartcard@post.cz",
-		Subject: "Smartcard",
-		Body: "Well that was easy!!"
-	}) 
-		.then(function (message) { 
-		alert("Mail has been sent successfully") 
-		});
-	
-    Email.send({ 
-    	Host: "smtp.seznam.cz",
-		Username: "smartcard@post.cz",
-		Password: "Smartcard123",
-		To: 'smartcard@post.cz',
-		From: "smartcard@post.cz",
-		Subject: "Smartcard",
-		Body: "Well that was easy!!"
-	}) 
-		.then(function (message) { 
-		alert("Mail has been sent successfully") 
-		});
+	var jmeno = document.getElementById("jmeno").value;
+	var prijmeni = document.getElementById("prijmeni").value;
+  	var ulice = document.getElementById("ulice").value;
+  	var PSC = document.getElementById("PSC").value;
+  	var mesto = document.getElementById("mesto").value;
+  	var email = document.getElementById("email").value;
+  	var telefon = document.getElementById("telefon").value;
+  	if (jmeno != "" && prijmeni != "" && ulice != "" && PSC != "" && mesto != "" && email != "" && telefon != "") {
+    	/*Email.send({
+    		Host: "smtp.seznam.cz",
+			Username: "smartcard@post.cz",
+			Password: "Smartcard123",
+			To: email,
+			From: "smartcard@post.cz",
+			Subject: "Smartcard",
+			Body: "Email.html"
+		}).then(alert("s"));
+    	/*Email.send({
+    		Host: "smtp.seznam.cz",
+			Username: "smartcard@post.cz",
+			Password: "Smartcard123",
+			To: 'smartcard@post.cz',
+			From: "smartcard@post.cz",
+			Subject: "Smartcard",
+			Body: ""
+		}).then(location.href='Informace.html')*/
+	}
+  	else {
+	  	window.alert("Chyba");
+  	}
 }
