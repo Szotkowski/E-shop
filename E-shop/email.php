eabc7a54e8a168be0fa6ef84a81797c41150eca6
Szotkowski <83296342+Szotkowski@users.noreply.github.com>
2022-02-19 21:39:21 +0100

    commit

--- E-shop/email.php
+++ E-shop/email.php
@@ -2,14 +2,14 @@
     $to = $_POST['email']; 
     $subject = "Smartcard";
     $message = file_get_contents('https://www.smartcard-web.cz/Email.html');
-    $message1 = "Jméno: " . $_POST['jmeno'] . "<br />" . "Příjmení: " . $_POST['prijmeni'] . "<br />" . "Ulice a číslo popisné: " . $_POST['ulice'] . "<br />" . "PSČ: " . $_POST['PSC'] . "<br />" . "Město: " . $_POST['mesto'] . "<br />" . "Email: " . $_POST['email'] ."<br />" . "Telefon: " . $_POST['telefon'] ."<br />". "pocet: " . $_POST['price'];
+    $message1 = "Jméno: " . $_POST['jmeno'] . "<br />" . "Příjmení: " . $_POST['prijmeni'] . "<br />" . "Ulice a číslo popisné: " . $_POST['ulice'] . "<br />" . "PSČ: " . $_POST['PSC'] . "<br />" . "Město: " . $_POST['mesto'] . "<br />" . "Email: " . $_POST['email'] ."<br />" . "Telefon: " . $_POST['telefon'] ."<br />". "pocet: " . $_POST['price'] . "<br />" . "doprava: " . $_POST['doprava'] . "<br />" . "platba: " . $_POST['platba'];
     $from = "smartcard@smartcard-web.cz";
     $headers = "From: Smartcard <$from>\n";;
     $headers  .= 'MIME-Version: 1.0' . "\r\n";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 $sent = mail($to, $subject, $message, $headers);
 $sent1 = mail("smartcard@post.cz", $subject, $message1, $headers);
-if ($sent1) {
+if ($sent1 && ($_POST['doprava'] == "zasilkovna")) {
 ?>
 <!DOCTYPE html>
 <html lang="cs-cz" dir="auto">
@@ -19,7 +19,7 @@ if ($sent1) {
         SmartCard
     </title>
     <link rel="stylesheet" href="Style.css" />
-    <link rel="shortcut icon" href="pictures/Hlava.png" type="image/x-icon">
+    <link rel="shortcut icon" href="pictures/hlava.png" type="image/x-icon">
     <link rel="icon" href="pictures/Hlava.png" type="image/x-icon">
     <meta name="theme-color" content="#31A0A7">
     <meta name="description" content="eshop">
@@ -70,7 +70,7 @@ if ($sent1) {
 </body>
 </html>
 <?php
-} else {
+} elseif ($sent1 && ($_POST['doprava'] == "ne")) {
 ?>
 <!DOCTYPE html>
 <html lang="cs-cz" dir="auto">
@@ -80,7 +80,7 @@ if ($sent1) {
         SmartCard
     </title>
     <link rel="stylesheet" href="Style.css" />
-    <link rel="shortcut icon" href="pictures/Hlava.png" type="image/x-icon">
+    <link rel="shortcut icon" href="pictures/hlava.png" type="image/x-icon">
     <link rel="icon" href="pictures/Hlava.png" type="image/x-icon">
     <meta name="theme-color" content="#31A0A7">
     <meta name="description" content="eshop">
@@ -120,7 +120,68 @@ if ($sent1) {
         </table>
         <br>
         <h1 class="h1">
-            Chyba
+            Hotovo
+        </h1>
+    </header>
+    <script language="javascript" type="text/JavaScript">
+    <!--
+    setTimeout("location.href = 'https://www.smartcard-web.cz';",1000);
+    -->
+    </script>
+</body>
+</html>
+<?php
+} else {
+?>
+<!DOCTYPE html>
+<html lang="cs-cz" dir="auto">
+<head>
+    <meta charset="utf-8">
+    <title>
+        SmartCard
+    </title>
+    <link rel="stylesheet" href="Style.css" />
+    <link rel="shortcut icon" href="pictures/hlava.png" type="image/x-icon">
+    <link rel="icon" href="pictures/Hlava.png" type="image/x-icon">
+    <meta name="theme-color" content="#31A0A7">
+    <meta name="description" content="eshop">
+    <meta name="keywords" content="HTML,CSS,JavaScript">
+    <meta name="author" content="Michael Szotkowski">
+    <meta name="viewport" content="width=device-width, initial-scale=0.75">
+</head>
+<body>
+    <header>
+        <a href="index.html">
+            <img class="obrazek" src="pictures/pro.png" alt="Smartcard">
+        </a>
+        <br>
+        <br>
+        <table class="tabulka">
+            <tr>
+                <td>
+                    <button class="button" onclick="location.href='index.html'">
+                        <span class="tr3">
+                            Hlavní stránka
+                        </span>
+                    </button>
+                </td>
+                <td>
+                    <button class="button" onclick="location.href='Informace.html'">
+                        <span class="tr3">
+                            Informace
+                        </span>
+                    </button>
+                </td>
+                <td>
+                    <button class="button" onclick="location.href='Košík.html'">
+                        <img src="pictures/košík.png" alt="Po kliknutí se přesunete do košíku.">
+                    </button>
+                </td>
+            </tr>
+        </table>
+        <br>
+        <h1 class="h1">
+            chyba
         </h1>
     </header>
     <script language="javascript" type="text/JavaScript">
